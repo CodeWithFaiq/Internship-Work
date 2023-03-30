ab123949843f7ea39cafc22790e0f960eb7072c7
Faiq-sp20-bcs-085 <113433823+Faiq-sp20-bcs-085@users.noreply.github.com>
2023-03-30 14:40:03 +0500

    further practice

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -8,14 +8,18 @@ const browser=await puppeteer.launch({
    userDataDir:'./temp'
 })
 
+let items=[];
 const page=await browser.newPage();
 await page.goto('https://www.amazon.com/b?node=16225009011&pf_rd_r=MSYCE46GSKDSY4W66TYS&pf_rd_p=5232c45b-5929-4ff0-8eae-5f67afd5c3dc&pd_rd_r=d29645e3-9d44-484a-9238-155c32475194&pd_rd_w=wuzgp&pd_rd_wg=gQxkG&ref_=pd_gw_unk');
- const data=await page.$$('h2');
+const data=await page.$$('.s-image');
 for(let i=0;i<data.length;i++){
-   let element=data[i];
-   let text=await page.evaluate(ele=>ele.textContent,element);
-   console.log(text);
+   let elementdiv=data[i];
+   let img=await page.evaluate(ele=>ele.getAttribute('src'),elementdiv);
+   items.push(img);
+   
 }
+
+
 await browser.close();
 
 }
