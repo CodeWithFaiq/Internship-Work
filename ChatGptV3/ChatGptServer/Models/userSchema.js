5c28240b6fa893679b3fe933784a5b30870e9921
CodeWithFaiq <125984152+CodeWithFaiq@users.noreply.github.com>
2023-04-05 13:50:39 +0500

    Add files via upload

---
+++ ChatGptV3/ChatGptServer/Models/userSchema.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+const userSchema=mongoose.Schema({
+    username:String,
+    password:String,
+    clientKey:String,
+    secretKey:String
+})
+
+
+const userModel=mongoose.model('users',userSchema);
+
+module.exports=userModel;
